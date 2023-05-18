a783c36574a54067fe599502af7e6f1accb863de
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-05-18 10:22:47 -0300

    feat: add team class

---
+++ tournament/src/Team.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Team {
+    private String name;
+    private ArrayList<Player> players;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+}
