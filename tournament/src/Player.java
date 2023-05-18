ce1d6351955515b39e4db72bde03a3aa7bc79ba3
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-05-18 10:18:55 -0300

    feat: add player class

---
+++ tournament/src/Player.java
@@ -0,0 +1,38 @@
+public class Player {
+    private int number;
+    private String name;
+    private String position;
+    private int goalCounter;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getGoalCounter() {
+        return goalCounter;
+    }
+
+    public void setGoalCounter(int goalCounter) {
+        this.goalCounter = goalCounter;
+    }
+}
