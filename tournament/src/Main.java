6658b2941a30ec3f840415021fc55fd1ecb666e2
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-05-22 10:38:46 -0300

    feat: add test team

--- tournament/src/Main.java
+++ tournament/src/Main.java
@@ -1,5 +1,21 @@
+import tournament.Player;
+import tournament.Team;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Player teacher = new Player("Teacher");
+        Player student1 = new Player("Student 1");
+        ArrayList<Player> playersA = new ArrayList<>();
+        playersA.add(teacher);
+        playersA.add(student1);
+
+        Team teamA = new Team("Equipo A", playersA);
+
+        System.out.println(teamA.getName());
+        teamA.getPlayers().forEach(player -> {
+            System.out.println(player.getName());
+        });
     }
 }
