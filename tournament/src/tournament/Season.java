7de9dab559ce17a6c4a8cc43c9bd5c8a76655f2c
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-05-22 09:51:56 -0300

    feat: add Season class

---
+++ tournament/src/tournament/Season.java
@@ -0,0 +1,52 @@
+package tournament;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Season {
+    private String name;
+    private Date startDate;
+    private Date endDate;
+    private ArrayList<Match> matches;
+    private ArrayList<Team> teams;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public ArrayList<Match> getMatches() {
+        return matches;
+    }
+
+    public void setMatches(ArrayList<Match> matches) {
+        this.matches = matches;
+    }
+
+    public ArrayList<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(ArrayList<Team> teams) {
+        this.teams = teams;
+    }
+}
