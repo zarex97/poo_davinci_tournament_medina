061e68ee3283749eb9c15e038e98073356c81fc2
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-06-01 09:17:11 -0300

    feat: add two teams and players

--- tournament/src/Main.java
+++ tournament/src/Main.java
@@ -7,15 +7,40 @@ public class Main {
     public static void main(String[] args) {
         Player teacher = new Player("Teacher");
         Player student1 = new Player("Student 1");
-        ArrayList<Player> playersA = new ArrayList<>();
-        playersA.add(teacher);
-        playersA.add(student1);
+      
+        Player sergio = new Player("Sergio");
+        Player jogher = new Player("Jogher");
+        Player paco = new Player("Paco");
+        Player tomas = new Player("Tomas");
+        Player isaac = new Player("Isaac");
+        Player franco = new Player("Franco");
+        Player isabel = new Player ("Isabel");
+        Player messi = new Player("Messi");
+        Player diMaria = new Player ("DiMaria");
+      
+      
+        //ArrayList<Player> playersA = new ArrayList<>();
+        //playersA.add(teacher);
+        //playersA.add(student1);
 
-        Team teamA = new Team("Equipo A", playersA);
-
-        System.out.println(teamA.getName());
-        teamA.getPlayers().forEach(player -> {
-            System.out.println(player.getName());
-        });
+        //Team teamA = new Team("Equipo A", playersA);
+        Team teamA = new Team("Equipo A");
+        teamA.addPlayer(messi);
+        teamA.addPlayer(diMaria);
+        teamA.addPlayer(isabel);
+        teamA.addPlayer(isaac);
+        
+        Team teamB = new Team("Equipo B");
+        teamB.addPlayer(tomas);  
+        teamB.addPlayer(jogher);
+        teamB.addPlayer(paco);
+        teamB.addPlayer(franco);
+        
+        //showMembers
+       teamA.showMembers();
+      
+        teamB.showMembers();
+  
+      System.out.println(tomas.showTeam());
     }
 }
