cd8d0cad4ddfd231442a1538fade50dbec927895
Tomas De Paulo <127751143+TomiDepa@users.noreply.github.com>
2023-06-12 09:48:51 -0300

    refactor: remove players references on teamB (#17)

--- tournament/src/Main.java
+++ tournament/src/Main.java
@@ -10,11 +10,7 @@ public class Main {
         Player student1 = new Player("Student 1");
       
         Player sergio = new Player("Sergio");
-        Player jogher = new Player("Jogher");
-        Player paco = new Player("Paco");
-        Player tomas = new Player("Tomas");
         Player isaac = new Player("Isaac");
-        Player franco = new Player("Franco");
         Player isabel = new Player ("Isabel");
         Player messi = new Player("Messi");
         Player diMaria = new Player ("DiMaria");
@@ -33,10 +29,10 @@ public class Main {
         teamA.addPlayer(isaac);
         
         Team teamB = new Team("Equipo B");
-        teamB.addPlayer(tomas);  
-        teamB.addPlayer(jogher);
-        teamB.addPlayer(paco);
-        teamB.addPlayer(franco);
+        teamB.addPlayer(new Player("Tomas"));
+        teamB.addPlayer(new Player("Jogher"));
+        teamB.addPlayer(new Player("Paco"));
+        teamB.addPlayer(new Player("Franco"));
         
         //showMembers
        teamA.showMembers();
