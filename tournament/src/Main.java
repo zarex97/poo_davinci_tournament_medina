ea2b52e441f74cd07d65c87b5a26259e346c8920
zarex97 <131675618+zarex97@users.noreply.github.com>
2023-06-26 10:47:07 -0300

    feat: add new Array of Teams and Array of matches (#37)
    * feat: add new Array of Teams and Array of matches
    
    * fix: erased duplicated snippet of code

--- tournament/src/Main.java
+++ tournament/src/Main.java
@@ -31,32 +31,46 @@ public class Main {
        teamB.showMembers();
   
         //System.out.println(tomas.showTeam());
+        ArrayList<Team> equiposCuarto= new ArrayList<Team>();
+        equiposCuarto.add(teamA);
+        equiposCuarto.add(teamB);
+        equiposCuarto.add(teamC);
+        equiposCuarto.add(teamD);
+        equiposCuarto.add(teamE);
+        equiposCuarto.add(teamF);
+        equiposCuarto.add(teamG);
+        equiposCuarto.add(teamH);
 
-        Match primerPartido = new Match(teamA, teamB);
-        Match segundoPartido=new Match(teamC,teamD);
-        Match tercerPartido=new Match(teamE,teamF);
-        Match cuartoPartido = new Match(teamG,teamH);
 
-        primerPartido.makeLocalGoal(1);
-        primerPartido.makeVisitantGoal(2);
-        primerPartido.makeVisitantGoal(3);
-        System.out.println(primerPartido.getMatchStatus());
+        ArrayList<Match> partidosCuarto = new ArrayList<Match>();
+        partidosCuarto.add(new Match(equiposCuarto.get(0), equiposCuarto.get(1)));
+        partidosCuarto.add(new Match(equiposCuarto.get(2), equiposCuarto.get(3)));
+        partidosCuarto.add(new Match(equiposCuarto.get(4), equiposCuarto.get(5)));
+        partidosCuarto.add(new Match(equiposCuarto.get(6), equiposCuarto.get(7)));
 
-        segundoPartido.makeLocalGoal(0);
-        System.out.println(segundoPartido.getMatchStatus());
 
-        tercerPartido.makeVisitantGoal(1);
-        tercerPartido.makeVisitantGoal(2);
-        tercerPartido.makeLocalGoal(0);
-        tercerPartido.makeLocalGoal(3);
-        tercerPartido.makeLocalGoal(2);
-        System.out.println(tercerPartido.getMatchStatus());
+        //Rearrange games based on new Match structure (partidosCuartos)
 
-        cuartoPartido.makeLocalGoal(2);
-        cuartoPartido.makeVisitantGoal(0);
-        cuartoPartido.makeVisitantGoal(1);
-        cuartoPartido.makeLocalGoal(3);
-        System.out.println(cuartoPartido.getMatchStatus());
+        //primerPartido.makeLocalGoal(1);
+        //primerPartido.makeVisitantGoal(2);
+        //primerPartido.makeVisitantGoal(3);
+        //System.out.println(primerPartido.getMatchStatus());
+
+        //segundoPartido.makeLocalGoal(0);
+        //System.out.println(segundoPartido.getMatchStatus());
+
+        //tercerPartido.makeVisitantGoal(1);
+        //tercerPartido.makeVisitantGoal(2);
+        //tercerPartido.makeLocalGoal(0);
+        //tercerPartido.makeLocalGoal(3);
+        //tercerPartido.makeLocalGoal(2);
+        //System.out.println(tercerPartido.getMatchStatus());
+
+        //cuartoPartido.makeLocalGoal(2);
+        //cuartoPartido.makeVisitantGoal(0);
+        //cuartoPartido.makeVisitantGoal(1);
+        //cuartoPartido.makeLocalGoal(3);
+        //System.out.println(cuartoPartido.getMatchStatus());
 
     }
 
